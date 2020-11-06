4f8b5c14d2e3982c4b9a2c6b568e5c1273655aa1
Turnyur <chukunah@gmail.com>
2020-11-06 12:35:33 +0100

    Laravel Modular architecture for large project

---
+++ Modules/Blog/Routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::prefix('blog')->group(function() {
+    Route::get('/', 'BlogController@index');
+    Route::get('/{test}', function($test){
+        return "Welcome to Laravel modular architecture, ".$test;
+    });
+});
